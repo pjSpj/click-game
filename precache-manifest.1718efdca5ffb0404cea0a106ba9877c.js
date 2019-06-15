0d6377093aebe71fbaaac7294d380a96c09ffd0c
pj song <pj@pjs-MacBook-Pro.local>
2019-06-15 01:05:16 -0400

    Updates

---
+++ precache-manifest.1718efdca5ffb0404cea0a106ba9877c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfcb2efe0eb467589f1ac3500ec1731",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "56716d4abc61a36ed5c4",
+    "url": "/click-game/static/css/main.09f04447.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/click-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "56716d4abc61a36ed5c4",
+    "url": "/click-game/static/js/main.70f50529.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
